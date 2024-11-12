50bd94c61565649487cff99d157e8604ea2913ef
Carlos <carlos.brunno97@gmail.com>
2024-11-12 09:48:41 -0300

    Create To-Do-List.Api base project

---
+++ To-Do-List.Api/To-Do-List.Application/Services/TaskItemService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using To_Do_List.Domain.Entities;
+using To_Do_List.Domain.Interfaces;
+
+namespace To_Do_List.Application.Services
+{
+    public class TaskItemService
+    {
+        private readonly ITaskItemRepository _taskitemRepository;
+
+        public TaskItemService(ITaskItemRepository taskitemRepository)
+        {
+            _taskitemRepository = taskitemRepository;
+        }
+
+        public Task<IEnumerable<TaskItem>> GetAllAsync() => _taskitemRepository.GetAllAsync();
+        public Task<TaskItem> GetByIdAsync(Guid id) => _taskitemRepository.GetByIdAsync(id);
+        public Task AddAsync(TaskItem taskitem) => _taskitemRepository.AddAsync(taskitem);
+        public Task UpdateAsync(TaskItem taskitem) => _taskitemRepository.UpdateAsync(taskitem);
+        public Task DeleteAsync(Guid id) => _taskitemRepository.DeleteAsync(id);
+    }
+}
---
+++ To-Do-List.Api/To-Do-List.Domain/Interfaces/ITaskItemRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using To_Do_List.Domain.Entities;
+
+namespace To_Do_List.Domain.Interfaces
+{
+    public interface ITaskItemRepository
+    {
+        Task<IEnumerable<TaskItem>> GetAllAsync();
+        Task<TaskItem> GetByIdAsync(Guid id);
+        Task AddAsync(TaskItem product);
+        Task UpdateAsync(TaskItem product);
+        Task DeleteAsync(Guid id);
+    }
+}
